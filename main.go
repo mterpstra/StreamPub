46016dd4ce06c336e8042f25dd520e3cf3976589
mterpstra <markterp@cloud.com>
2016-02-22 08:07:01 -0500

    Working changes for demo

--- main.go
+++ main.go
@@ -1,62 +1,102 @@
 package main
 
 import (
-	"github.com/garyburd/redigo/redis"
-	"github.com/gorilla/mux"
-	"github.com/gorilla/websocket"
+	"crypto/rand"
+	"fmt"
+	"io/ioutil"
 	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gorilla/mux"
+	redis "gopkg.in/redis.v3"
 )
 
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
+var redisClient *redis.Client
+
+func uuid() string {
+	// generate 32 bits timestamp
+	unix32bits := uint32(time.Now().UTC().Unix())
+
+	buff := make([]byte, 12)
+
+	numRead, err := rand.Read(buff)
+
+	if numRead != len(buff) || err != nil {
+		panic(err)
+	}
+
+	return fmt.Sprintf("%x-%x-%x-%x-%x-%x", unix32bits, buff[0:2], buff[2:4], buff[4:6], buff[6:8], buff[8:])
 }
 
-var redisConnection redis.Conn
+func handlePublish(w http.ResponseWriter, r *http.Request) {
 
-func handleSubscription(w http.ResponseWriter, r *http.Request) {
-	println("handleSubscription")
+	vars := mux.Vars(r)
+	documentID := vars["documentid"]
 
-	conn, err := upgrader.Upgrade(w, r, nil)
+	payload, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		println(err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
+	reply := redisClient.Publish(documentID, string(payload))
+	_, err = reply.Result()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func handlePublishTTL(w http.ResponseWriter, r *http.Request) {
+
 	vars := mux.Vars(r)
-	device := vars["device"]
-	session := vars["session"]
+	documentID := vars["documentid"]
+	ttl := vars["ttl"]
 
-	println("handleSubscription, listening...")
-	for {
-		messageType, p, err := conn.ReadMessage()
-		if err != nil {
-			println(err.Error())
-			return
-		}
+	payload, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-		println("---------------------------------------------------")
-		println("redisConnection:", redisConnection)
-		println("device:", device)
-		println("session:", session)
-		println("msgtype:", messageType)
-		println("message:", string(p))
+	uuID := uuid()
+	key := fmt.Sprintf("data:%s:%s", documentID, uuID)
+	reply := redisClient.Set(key, payload, 0)
 
-		redisConnection.Do("PUBLISH", session, p)
+	seconds, err := strconv.ParseUint(ttl, 10, 64)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	key = fmt.Sprintf("ttl:%s:%s", documentID, uuID)
+	reply = redisClient.Set(key, "", time.Second*time.Duration(seconds))
+	_, err = reply.Result()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	key = fmt.Sprintf("data:%s:%s", documentID, uuID)
+	reply = redisClient.Set(key, payload, 0)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 }
 
 func main() {
 
 	var err error
-	redisConnection, err = redis.Dial("tcp", "localhost:6379")
-	if err != nil {
-		println(err.Error())
-	}
-	defer redisConnection.Close()
+
+	options := &redis.Options{Network: "tcp", Addr: "localhost:6379"}
+	redisClient = redis.NewClient(options)
+	defer redisClient.Close()
 
 	r := mux.NewRouter()
-	r.HandleFunc("/pub/{device}/{session}", handleSubscription)
+	r.HandleFunc("/pub/{documentid}", handlePublish)
+	r.HandleFunc("/pub/{documentid}/{ttl}", handlePublishTTL)
 	r.Handle("/{rest}", http.FileServer(http.Dir(".")))
 	http.Handle("/", r)
 	err = http.ListenAndServe(":2000", nil)
